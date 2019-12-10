42562ea5f111de8f3acb65a705e26941535a08cd
piotrjaromin <pjaromin89@gmail.com>
2019-12-10 08:06:15 +0100

    orignal xml2js added for comparison

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -4,6 +4,7 @@ const { readFileSync } = require('fs');
 const Benchmark = require('benchmark');
 
 const { parseXmlString, parseString } = require('../index');
+const { parseString: originalParseString } = require('xml2js');
 const simpleXml = readFileSync('./test/fixtures/simple.xml', 'utf-8').toString();
 
 const suite = new Benchmark.Suite;
@@ -13,6 +14,11 @@ suite.add('parseXmlString#simple', async () => {
 })
 .add('parseString#simple', async () => {
     await new Promise(resolve => parseString(simpleXml, async () => {
+        resolve();
+    }));
+})
+.add('originalParseString#simple', async () => {
+    await new Promise(resolve => originalParseString(simpleXml, async () => {
         resolve();
     }));
 })
