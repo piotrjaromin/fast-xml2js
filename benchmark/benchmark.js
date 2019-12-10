08592f514cee1106a2b1fc268190bb0f678954dc
piotrjaromin <pjaromin89@gmail.com>
2019-12-10 08:08:17 +0100

    Fix

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -25,4 +25,8 @@ suite.add('parseXmlString#simple', async () => {
 .on('cycle', function(event) {
   console.log(String(event.target));
 })
+.on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
 .run({ 'async': true });
+
